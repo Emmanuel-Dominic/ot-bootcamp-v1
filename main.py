bd4293adc5dee1964f32fe1d6dec85afe622743e
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-05-14 08:57:21 +0300

    implement flask api with databases

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
 from controllers.controller import is_admin
 from views.view import student_blueprint
+from views.user_view import user_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(student_blueprint)
+app.register_blueprint(user_blueprint)
 
 
 @app.route('/')
